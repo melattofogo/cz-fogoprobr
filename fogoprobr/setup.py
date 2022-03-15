8dd7368c0d3e9484a27be3d9e6d21c689d7f348a
Fogo <melatto.fogo@live.com>
2022-03-15 20:34:36 -0300

    feat(setup.py): adiciona arquivo de setup para criacao do package commitizen-cz-fogoprobr

---
+++ fogoprobr/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name="commitizen-cz-fogoprobr",
+    version="0.1.0",
+    py_modules=["cz_fogoprobr"],
+    license="MIT",
+    long_description="Commitizen personalizado por melattofogo",
+    long_description_content_type="text/x-rst",
+    install_requires=["commitizen>=2.21.2"],
+)
